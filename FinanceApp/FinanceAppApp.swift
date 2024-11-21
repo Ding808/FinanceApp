0942fe872314d475a768a4673c5b5e18310164bc
Yueyang Ding <dingyeuyang@gmail.com>
2024-11-21 14:30:43 -0500

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Yueyang Ding on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

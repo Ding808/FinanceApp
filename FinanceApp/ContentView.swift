fb306cac5e416402054bb937c91a0a1ce0917816
MohammadRaja <rajmoham@sheridancollege.ca>
2024-11-21 15:48:37 -0500

    My first commit

--- FinanceApp/ContentView.swift
+++ FinanceApp/ContentView.swift
@@ -12,6 +12,7 @@ struct Product: Identifiable {
     let title: String
     var revenue: Double
 }
+//Test message 
 
 struct FinanceAppView: View {
     private let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium) // Initialize haptic feedback
